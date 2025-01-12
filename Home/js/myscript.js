96aa4e10abda3adb9f28ce50640151df23ec5b85
Marianne Ellise P. Lama <lems.mariannelama@gmail.com>
2025-01-12 23:55:47 +0800

    Delete Home/js directory

--- Home/js/myscript.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }  
-
-  function myFunction2() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-    const element = document.getElementsByTagName("p");
-document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
-  }  
