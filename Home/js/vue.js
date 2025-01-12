bfb2c7ec040fb164b826e470964ea4eb73366304
Marianne Ellise P. Lama <lems.mariannelama@gmail.com>
2025-01-13 06:40:13 +0800

    Update vue.js

--- Home/js/vue.js
+++ Home/js/vue.js
@@ -1,1 +1,12 @@
-
+const app = Vue.createApp({
+    data() {
+      return {
+        text: ''
+      }
+    },
+    methods: {
+      changeText() {
+        this.text = 'Hello World!'
+      }
+    }
+  })
